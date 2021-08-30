bcdcd4e500c0c2bc3602c37daea8c291f626e131
LeoMxrrill <lmerrill4@huskers.unl.edu>
2021-08-30 22:53:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Leo Merrill
+ * Date: 2021/08/30
+ *
+ *  A simple Name printing program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+     printf("Leo Merrill!\n");
+
+     return 0;
+ }
